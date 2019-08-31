e8dcb132761b1787da0ba6c284db50ebc010e62d
Niels Clayton <nielsdanielclayton@gmail.com>
2019-09-01 09:21:43 +1200

    220 assignment

---
+++ Tri 2/ECEN 220/Assignment_5.m
@@ -0,0 +1,149 @@
+%% a
+clear
+clc
+
+T = 7;
+n = 0:1:6
+w = 2.*pi./T;
+magnitude = [];             % Array of magnitudes
+phase = [];                 % Array of phases
+
+x_n = 0;
+
+for k = (0:6) 
+    ak = (1/T.*((1-exp(-5j.*k.*w))./(1 - exp(-1j.*k.*w))));
+    
+    
+    if k == 0
+        x_n = x_n + (5/7).*exp(1j.*k.*w.*n);
+        magnitude = [magnitude, abs(5/7)];
+        phase = [phase, rad2deg(angle(5/7))];
+    end
+    
+    if k ~= 0 
+        x_n = x_n + (ak .* exp(1j.*k.*w.*n));
+        magnitude = [magnitude, abs(ak)];
+        phase = [phase, rad2deg(angle(ak))];
+    end
+end
+
+figure(1)
+stem(n,x_n);
+
+figure(2)
+subplot(2,1,[1 2]);
+% Plot magnitudes
+p2 = subplot(2,1,1);
+k10 = 0:6;
+stem(k10, magnitude);
+xlabel(p2,'k');
+ylabel(p2,'Magnitude');
+legend(p2,'Magnitudes of a_k')
+
+% Plot phases 
+p3 = subplot(2,1,2);
+k10 = 0:6;
+stem(k10, phase);
+xlabel(p3,'k');
+ylabel(p3,'phase [deg]');
+legend(p3,'Phases of a_k');
+
+%% b
+clear
+clc
+
+T = 6;
+n = 0:1:5
+w = 2.*pi./T;
+magnitude = [];             % Array of magnitudes
+phase = [];                 % Array of phases
+
+x_n = 0;
+
+for k = (0:5) 
+    ak = (1/T.*((1-exp(-4j.*k.*w))./(1 - exp(-1j.*k.*w))));
+    
+    
+    if k == 0
+        x_n = x_n + (4/6).*exp(1j.*k.*w.*n);
+        magnitude = [magnitude, abs(4/6)];
+        phase = [phase, rad2deg(angle(4/6))];
+    end
+    
+    if k ~= 0 
+        x_n = x_n + (ak .* exp(1j.*k.*w.*n));
+        magnitude = [magnitude, abs(ak)];
+        phase = [phase, rad2deg(angle(ak))];
+    end
+end
+
+figure(1)
+stem(n,x_n);
+
+figure(2)
+subplot(2,1,[1 2]);
+% Plot magnitudes
+p2 = subplot(2,1,1);
+k10 = 0:5;
+stem(k10, magnitude);
+xlabel(p2,'k');
+ylabel(p2,'Magnitude');
+legend(p2,'Magnitudes of a_k')
+
+% Plot phases 
+p3 = subplot(2,1,2);
+k10 = 0:5;
+stem(k10, phase);
+xlabel(p3,'k');
+ylabel(p3,'phase [deg]');
+legend(p3,'Phases of a_k');
+
+%% c
+clear
+clc
+
+T = 7;
+n = 0:1:6
+w = 2.*pi./T;
+magnitude = [];             % Array of magnitudes
+phase = [];                 % Array of phases
+
+x_n = 0;
+
+for k = (0:6) 
+    ak = (1/T.*((1-exp(-5j.*k.*w))./(1 - exp(-1j.*k.*w))));
+    
+    
+    if k == 0
+        x_n = x_n + (5/7).*exp(1j.*k.*w.*n);
+        magnitude = [magnitude, abs(5/7)];
+        phase = [phase, rad2deg(angle(5/7))];
+    end
+    
+    if k ~= 0 
+        x_n = x_n + (ak .* exp(1j.*k.*w.*n));
+        magnitude = [magnitude, abs(ak)];
+        phase = [phase, rad2deg(angle(ak))];
+    end
+end
+
+figure(1)
+stem(n,x_n);
+
+figure(2)
+subplot(2,1,[1 2]);
+% Plot magnitudes
+p2 = subplot(2,1,1);
+k10 = 0:6;
+stem(k10, magnitude);
+xlabel(p2,'k');
+ylabel(p2,'Magnitude');
+legend(p2,'Magnitudes of a_k')
+
+% Plot phases 
+p3 = subplot(2,1,2);
+k10 = 0:6;
+stem(k10, phase);
+xlabel(p3,'k');
+ylabel(p3,'phase [deg]');
+legend(p3,'Phases of a_k');
