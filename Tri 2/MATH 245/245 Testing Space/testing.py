b7ca77b5ca49bdac78e3d0831ffa2ef891b4ad81
Niels Clayton <nielsdanielclayton@gmail.com>
2019-09-22 10:32:55 +1200

    245

--- Tri 2/MATH 245/245 Testing Space/testing.py
+++ Tri 2/MATH 245/245 Testing Space/testing.py
@@ -1,4 +1,105 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.interpolate as sci
+from decimal import Decimal
+
+
+
+f = lambda x:1/(1+x**2)
+int_f = 2*np.arctan(5)
+
+def midpointquad(func, a, b, N):
+    x = np.linspace(a, b, N)
+    total = 0
+    for k in range(0, len(x)-1):
+        total += ((x[k+1]-x[k])*func((x[k]+x[k+1])/2))
+    return total
+
+
+# print("{:5s} {:5s} {:17s} {:15s}".format("N","h", "Midpoint", "Error"))
+# for n in (11, 101, 1001, 10001):
+#     mid = midpointquad(f, -5, 5, n)
+#     print("{:5d} {:.3f} {:.15f} {:.2E}".format(n, (10 / (n - 1)), mid, np.abs(int_f-mid)))
+
+
+
+# func1 = midpointquad(lambda x: 1     , 0, 1, 2)
+# func2 = midpointquad(lambda x: 2*x   , 0, 1, 2)
+# func3 = midpointquad(lambda x: 3*x**2, 0, 1, 2)
+# func4 = midpointquad(lambda x: 4*x**3, 0, 1, 2)
+#
+# print("{:5s} {:8s} {:15s}".format("func", "Midpoint", "Error"))
+# print("{:5s} {:.6f} {:.2f}".format("1",    func1, np.abs(1 - func1)))
+# print("{:5s} {:.6f} {:.2f}".format("2x",   func2, np.abs(1 - func2)))
+# print("{:5s} {:.6f} {:.2f}".format("3x^2", func3, np.abs(1 - func3)))
+# print("{:5s} {:.6f} {:.2f}".format("4x^3", func4, np.abs(1 - func4)))
+
+def trapezoid(func, a, b, N):
+    x = np.linspace(a, b, N)
+    total = 0
+    h = np.abs(b-a)/(N-1)
+
+
+    for k in range(0, len(x)-1):
+        x1 = func(x[k])
+        x2 = func(x[k+1])
+
+        total += ((x1 + x2)/2) * h
+    return total
+
+
+# print("{:5s} {:5s} {:17s} {:15s}".format("N","h", "Midpoint", "Error"))
+# for n in (11, 101, 1001, 10001):
+#     mid = trapezoid(f, -5, 5, n)
+#     print("{:5d} {:.3f} {:.15f} {:.2E}".format(n, (10 / (n - 1)), mid, np.abs(int_f-mid)))
+#
+#
+#
+# func1 = trapezoid(lambda x: 1     , 0, 1, 2)
+# func2 = trapezoid(lambda x: 2*x   , 0, 1, 2)
+# func3 = trapezoid(lambda x: 3*x**2, 0, 1, 2)
+# func4 = trapezoid(lambda x: 4*x**3, 0, 1, 2)
+#
+#
+# print("{:5s} {:8s} {:15s}".format("func", "Midpoint", "Error"))
+# print("{:5s} {:.6f} {:.2f}".format("1",    func1, np.abs(1 - func1)))
+# print("{:5s} {:.6f} {:.2f}".format("2x",   func2, np.abs(1 - func2)))
+# print("{:5s} {:.6f} {:.2f}".format("3x^2", func3, np.abs(1 - func3)))
+# print("{:5s} {:.6f} {:.2f}".format("4x^3", func4, np.abs(1 - func4)))
+
+def GaussQuadrature(func, a, b, N):
+    x, w = np.polynomial.legendre.leggauss(N)
+    total = 0
+
+    for n in range(N):
+        total += (w[n] * func( (((b-a)/2) * x[n]) + ((a+b)/2) ) )
+
+    return (total*((b-a)/2))
+
+# func1 = GaussQuadrature(lambda x: 3*x**2, 0, 1, 2)
+# func2 = GaussQuadrature(lambda x: 4*x**3, 0, 1, 2)
+# func3 = GaussQuadrature(lambda x: 5*x**4, 0, 1, 2)
+# func4 = GaussQuadrature(lambda x: 6*x**5, 0, 1, 2)
+# func5 = GaussQuadrature(lambda x: 7*x**6, 0, 1, 2)
+#
+# func6 = GaussQuadrature(lambda x: 3*x**2, 0, 1, 3)
+# func7 = GaussQuadrature(lambda x: 4*x**3, 0, 1, 3)
+# func8 = GaussQuadrature(lambda x: 5*x**4, 0, 1, 3)
+# func9 = GaussQuadrature(lambda x: 6*x**5, 0, 1, 3)
+# func10 = GaussQuadrature(lambda x: 7*x**6, 0, 1, 3)
+#
+# print("{:5s} {:8s} {:15s}".format("func", "Error N = 2", "Error N = 3"))
+# print("{:5s} {:.6f}    {:.4f}".format("3x^2", np.abs(1 - func1), np.abs(1-func6)))
+# print("{:5s} {:.6f}    {:.4f}".format("4x^3", np.abs(1 - func2), np.abs(1-func7)))
+# print("{:5s} {:.6f}    {:.4f}".format("5x^4", np.abs(1 - func3), np.abs(1-func8)))
+# print("{:5s} {:.6f}    {:.4f}".format("6x^5", np.abs(1 - func4), np.abs(1-func9)))
+# print("{:5s} {:.6f}    {:.4f}".format("7x^6", np.abs(1 - func5), np.abs(1-func10)))
+
+
+f = lambda x:1/(1+x**2)
+int_f = 2*np.arctan(5)
+
+print("{:5s} {:17s} {:15s}".format("N","Gauss-Lehendre", "Error"))
+for n in (3, 7, 11, 15):
+    mid = GaussQuadrature(f, -5, 5, n)
+    print("{:5d} {:.15f} {:.10f}".format(n, mid, np.abs(int_f-mid)))
